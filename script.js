61bca0c21f39c9f1e9578a1e106a0be8c0ad8764
Arman Ul Haq <72503393+armanulhaq@users.noreply.github.com>
2024-10-30 15:53:06 +0530

    add basic functionality

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 let boxes = document.querySelectorAll(".box")
 let turn1 = true
+let winning_patterns = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+]
+let gameStatus = document.getElementById("game-status")
 
 boxes.forEach(box => {
     box.addEventListener("click", ()=>{
@@ -11,5 +22,21 @@ boxes.forEach(box => {
             turn1 = true
         }
         box.disabled = true;
+        checkWinner()
     });
 });
+
+const checkWinner = () => {
+    for(let pattern of winning_patterns){
+        let pos1 = boxes[pattern[0]].innerText
+        let pos2 = boxes[pattern[1]].innerText
+        let pos3 = boxes[pattern[2]].innerText
+        if(pos1!='' && pos2!='' && pos3!='') {
+
+            if(pos1 === pos2 && pos2=== pos3) {
+                console.log("winner")
+                gameStatus.innerHTML = "Winner"
+            }
+        }
+    }
+}
